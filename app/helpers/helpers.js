bbf8c8f00c01d6052e20fc6fab9e856ed7903120
Ahmed Rabie Mahmoud Mohamed <ahmed.rabie.mahmoud.96@gmail.com>
2023-11-18 15:30:12 +0200

    Response handler helper method implemented

---
+++ app/helpers/helpers.js
@@ -0,0 +1,12 @@
+
+class Herlper {
+    static responseHandler = (res, statusCode, apiStatus, data, message) => {
+        res.status(statusCode).send({
+            apiStatus,
+            data,
+            message
+        })
+    }
+
+}
+module.exports = Herlper
