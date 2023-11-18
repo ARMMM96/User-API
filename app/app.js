24addb999a08b7a94570ec0657e6e162e4ee7a8b
Ahmed Rabie Mahmoud Mohamed <ahmed.rabie.mahmoud.96@gmail.com>
2023-11-18 15:32:52 +0200

    User routes linked to the app

--- app/app.js
+++ app/app.js
@@ -3,15 +3,18 @@ const cors = require("cors");
 const connectDB = require("./database/connection");
 const app = express();
 
+
+app.use(express.json())
 app.use(cors());
 
 
 connectDB()
 
+const userRoutes = require('./routes/user.routes')
 
 
 
-
+app.use("/user", userRoutes)
 
 app.all("*", (req, res) => {
     res.status(404).send({
